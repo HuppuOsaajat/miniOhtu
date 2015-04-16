2a0bac42efc8dde5672bc3ebc8c4add320b345d9
Rapsutin <juho.lamminmaki@gmail.com>
2015-04-16 15:18:15 +0300

    10 viitetyypin required ja optional fieldsien määrittely

--- app/models/bibref.rb
+++ app/models/bibref.rb
@@ -10,6 +10,38 @@ class Bibref < ActiveRecord::Base
       book: {
           required: [:author, :title, :publisher, :year],
           optional: [:volume, :series, :address, :edition, :month, :note, :key]
+      },
+      inproceedings: {
+          required: [:author, :title, :booktitle, :publisher, :year],
+          optional: [:editor, :volume, :series, :pages, :address, :month, :organization, :publisher, :note, :key]
+      },
+      misc: {
+          required: [],
+          optional: [:author, :title, :howpublished, :month, :year, :note, :key]
+      },
+      mastersthesis: {
+          required: [:author, :title, :school, :year],
+          optional: [:type, :address, :month, :note, :key]
+      },
+      phdthesis: {
+          required: [:author, :title, :school, :year],
+          optional: [:type, :address, :month, :note, :key]
+      },
+      unpublished: {
+          required: [:author, :title, :note],
+          optional: [:month, :year, :key]
+      },
+      proceedings: {
+          required: [:title, :year],
+          optional: [:editor, :volume, :series, :address, :month, :publisher, :organization, :key]
+      },
+      techreport: {
+          required: [:author, :title, :institution, :year],
+          optional: [:type, :number, :address, :month, :note, :key]
+      },
+      booklet: {
+          required: [:title],
+          optional: [:author, :howpublished, :address, :month, :year, :note, :key]
       }
   }
 
