dc346b0d9b40defd09f4a19785c900f0d590ab4f
jruo <jruo@users.noreply.github.com>
2015-04-16 14:42:04 +0300

    assosiaatiotalu referenssityypeille ja *ehkä* toimivat metodit

--- app/models/bibref.rb
+++ app/models/bibref.rb
@@ -1,2 +1,21 @@
 class Bibref < ActiveRecord::Base
+
+  @reference_types = {
+      article: {
+          required: [:author, :title, :journal, :year, :volume],
+          optional: [:number, :pages, :month, :note, :key]
+      },
+      book: {
+          required: [:author, :title, :publisher, :year],
+          optional: [:volume, :series, :address, :edition, :month, :note, :key]
+      }
+  }
+
+  def get_required_fields
+    @reference_types[:reftype][:required]
+  end
+
+  def get_optional_fields
+    @reference_types[:reftype][:optional]
+  end
 end
