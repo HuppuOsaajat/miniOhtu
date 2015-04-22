de1bd66d05409eb65b81b0035c0af3621ecce693
Rapsutin <juho.lamminmaki@gmail.com>
2015-04-22 21:06:55 +0300

    Ääkköset ja kumppanit

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,6 +2,11 @@ class Field < ActiveRecord::Base
   belongs_to :bibref
 
   def to_bibtex
-    bibtex = "#{name} = {#{content}},"
+    unformatted_bibtex = "#{name} = {#{content}},"
+    formatted_bibtex = format_special_characters(unformatted_bibtex)
+  end
+
+  def format_special_characters(unformatted_bibtex)
+    unformatted_bibtex.gsub(/ä/, '\"{a}').gsub(/ö/, '\"{o}').gsub(/å/, '\r{a}')
   end
 end
--- test/models/field_test.rb
+++ test/models/field_test.rb
@@ -13,4 +13,29 @@ class FieldTest < ActiveSupport::TestCase
   test 'field is converted into bibtex correctly' do
     assert_equal "author = {#{@author_name}}," , @field.to_bibtex
   end
+
+  test 'field with ä is converted correctly' do
+    test_string = 'author = {ärinä},'
+    assert_equal 'author = {\"{a}rin\"{a}},', @field.format_special_characters(test_string)
+    end
+
+  test 'field with ö is converted correctly' do
+    test_string = 'author = {örinö},'
+    assert_equal 'author = {\"{o}rin\"{o}},', @field.format_special_characters(test_string)
+  end
+
+  test 'field with ö and ä is converted correctly' do
+    test_string = 'author = {örinä},'
+    assert_equal 'author = {\"{o}rin\"{a}},', @field.format_special_characters(test_string)
+    end
+
+  test 'field with å is converted correctly' do
+    test_string = 'author = {årinå},'
+    assert_equal 'author = {\r{a}rin\r{a}},', @field.format_special_characters(test_string)
+    end
+
+  test 'field with å, ö, and ä is converted correctly' do
+    test_string = 'author = {örinåä},'
+    assert_equal 'author = {\"{o}rin\r{a}\"{a}},', @field.format_special_characters(test_string)
+  end
 end
