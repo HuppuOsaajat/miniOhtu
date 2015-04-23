2c69a0084c8c588d9490a7848f835f2c2ec10529
jruo <jruo@users.noreply.github.com>
2015-04-23 16:54:57 +0300

    yksikkötestit haulle

--- test/models/bibref_test.rb
+++ test/models/bibref_test.rb
@@ -26,4 +26,33 @@ class BibrefTest < ActiveSupport::TestCase
     assert opt_fields.include? :note
     assert opt_fields.include? :key
   end
+
+  test 'search results are correct' do
+    article = Bibref.create(shortname: 'artikkeli', reftype: :article)
+    article.set_field_value(:author, 'Juho Lamminmäki')
+    article.set_field_value(:title, 'Näin teet kotikaljaa')
+
+    book = Bibref.create(shortname: 'kirja', reftype: :book)
+    book.set_field_value(:author, 'Etunimi Sukunimi')
+    book.set_field_value(:title, 'Otsikko')
+
+    search1 = Bibref.search_results('Juho')
+    assert search1.include? article
+    assert !(search1.include? book)
+
+    search2 = Bibref.search_results('kalja')
+    assert search2.include? article
+    assert !(search2.include? book)
+
+    search3 = Bibref.search_results('ETUNIMI SUKUNIMI')
+    assert search3.include? book
+    assert !(search3.include? article)
+
+    search4 = Bibref.search_results('Ei vastaa mitään')
+    assert search4.empty?
+
+    search5 = Bibref.search_results('kirj')
+    assert search5.include? book
+    assert !(search5.include? article)
+  end
 end
