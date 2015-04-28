8f85f00c02ff2bf5d46784b08eebd3a02ce0f786
wadror <wadror@wadror-VirtualBox.(none)>
2015-04-28 20:09:27 +0300

    kurkku

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,12 +1,12 @@
 
 
 When(/^the user types an existing word on field$/) do
-  pending
+  fill_in 'Search:', :with => @article.get_field(:year).content
 
 end
 
 When(/^the user types an nonexistent word on field$/) do
-  pending
+  fill_in 'Search:', :with => 'trololo'
 
 end
 
